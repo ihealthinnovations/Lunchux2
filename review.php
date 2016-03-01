57b9b44bca2d6e96ef3615f516303f4cd6a5ffc6
S Kumar <skumar@ihealthventures.com>
2016-03-01 17:26:09 -0500

    First version

---
+++ review.php
@@ -0,0 +1,77 @@
+<?php /*Other code*/ ?>
+
+<h2>Form Submission Data</h2>
+<form id="transparent_redirect_form" name="transparent_redirect_form" action="">
+<table>
+<tbody>
+<tr>
+<td><b>First Name</b></td>
+<td>
+<input type="text" id="fname" name="fname" value="<?php echo $_POST['fname1']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>MI</b></td>
+<td>
+<input type="text" id="md" name="signup_password"  value="<?php echo $_POST['mi1']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>Last Name</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['lname1']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>Case Number</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['csn']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>Child Income</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['ci']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>Name of Adult Household Members</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['hm1']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>Street Address</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['street']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>City</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['city']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>State</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['state']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>Zip</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['zip']; ?>">
+</td>
+</tr>
+<tr>
+<td><b>Signature of adult completing form</b></td>
+<td>
+<input type="text" id="md" name="lname"  value="<?php echo $_POST['saf']; ?>">
+</td>
+</tr>
+</tbody>
+</table>
+
+</form>
+<?php /*other code*/ ?>
